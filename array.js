8180eeabdd9e94e28cb8bd9b196186ddec44d947
nirwanrah <nirwanrah@gmail.com>
2020-08-18 13:58:02 +0700

    coba lagi

---
+++ array.js
@@ -0,0 +1,15 @@
+let newYearsResolutions = ['Keep a journal',
+    'Take a falconry class',
+    'Learn to juggle'];
+console.log(newYearsResolutions);
+
+const famousSayings = ['Fortune favors the brave.',
+    'A joke is a very serious thing.',
+    'Where there is love there is life.'];
+var listItem = famousSayings[0]; 
+console.log(listItem);
+console.log(famousSayings[2]); //element ke 2
+console.log(famousSayings[3]); //undefined
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados'; //ubah tomatoes jadi avocadoes
